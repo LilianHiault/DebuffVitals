ba26c5b86eef697868bf9ad8134ef1bc49a09e98
Lilian Hiault <34962237+LilianHiault@users.noreply.github.com>
2020-06-06 16:27:07 +0200

    Menu translated

--- DebuffVitalsFr/Menu.lua
+++ DebuffVitalsFr/Menu.lua
@@ -2,62 +2,62 @@ local DEBUG_ENABLED = DEBUG_ENABLED
 -- ------------------------------------------------------------------------
 -- Menu and Menu Utilities
 -- ------------------------------------------------------------------------
-TargetFrameMenu = class (Turbine.UI.ContextMenu)        
+TargetFrameMenu = class (Turbine.UI.ContextMenu)
 
-function TargetFrameMenu:Constructor() 
+function TargetFrameMenu:Constructor()
     Turbine.UI.ContextMenu.Constructor(self)
 
     local menuItems=self:GetItems()
 
-    menuItems:Add(Turbine.UI.MenuItem("New Target"))
+    menuItems:Add(Turbine.UI.MenuItem("Nouvelle cible"))
     menuItems:Get(1).Click = function(sender, args)
         AddNewTarget()
     end
 
-    menuItems:Add(Turbine.UI.MenuItem("Remove Target"))
+    menuItems:Add(Turbine.UI.MenuItem("Supprimer cible"))
     menuItems:Get(2).Click = function(sender, args)
         RemoveTarget(self.invoker.ID)
     end
 
-    menuItems:Add(Turbine.UI.MenuItem("Effects"))
+    menuItems:Add(Turbine.UI.MenuItem("Effets"))
 
-    menuItems:Add(Turbine.UI.MenuItem("Show", true))
+    menuItems:Add(Turbine.UI.MenuItem("Afficher", true))
     local showItems = menuItems:Get(4):GetItems()
 
-    showItems:Add(Turbine.UI.MenuItem("Effects"))
+    showItems:Add(Turbine.UI.MenuItem("Effets"))
     showItems:Get(1).Click = function(sender, args)
         self.invoker.ShowEffects = not self.invoker.ShowEffects
         self.invoker:SetEnabledEffects()
         self.invoker:Resize()
     end
 
-    showItems:Add(Turbine.UI.MenuItem("Morale"))
+    showItems:Add(Turbine.UI.MenuItem("Moral"))
     showItems:Get(2).Click = function(sender, args)
         self.invoker.ShowMorale = not self.invoker.ShowMorale
         self.invoker:Resize()
     end
 
-    showItems:Add(Turbine.UI.MenuItem("Power"))    
+    showItems:Add(Turbine.UI.MenuItem("Puissance"))
     showItems:Get(3).Click = function(sender, args)
-        self.invoker.ShowPower = not self.invoker.ShowPower 
+        self.invoker.ShowPower = not self.invoker.ShowPower
         self.invoker:Resize()
     end
 
     menuItems:Add(Turbine.UI.MenuItem("Options", true))
     local optionsItems = menuItems:Get(5):GetItems()
-    optionsItems:Add(Turbine.UI.MenuItem("Lock Position"))
+    optionsItems:Add(Turbine.UI.MenuItem("Sauvegarder position"))
     optionsItems:Get(1).Click = function(sender, args)
         LockedPosition = not LockedPosition
     end
-    optionsItems:Add(Turbine.UI.MenuItem("Save frame position"))
+    optionsItems:Add(Turbine.UI.MenuItem("Sauvegarder position de la fênetre"))
     optionsItems:Get(2).Click = function(sender, args)
         SaveFramePositions = not SaveFramePositions
-    end    
-    
-    optionsItems:Add(Turbine.UI.MenuItem("Save settings "))
+    end
+
+    optionsItems:Add(Turbine.UI.MenuItem("Sauvegarder paramètres "))
     optionsItems:Get(3).Click = function(sender, args)
         SaveSettings()
-    end    
+    end
 end
 
 function TargetFrameMenu:CreateEffectsMenu()
@@ -68,7 +68,7 @@ function TargetFrameMenu:CreateEffectsMenu()
     for k = 1, #EffectsSet do
         effect = Turbine.UI.MenuItem(EffectsSet[k][2], true, true)
         effect.id = k
-        effect.Click = function(sender, args)       
+        effect.Click = function(sender, args)
             self.invoker.EnabledEffectsToggles[k][2] = not self.invoker.EnabledEffectsToggles[k][2]
             self.invoker:SetEnabledEffects()
             self.invoker:Resize()
